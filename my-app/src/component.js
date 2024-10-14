ea9dbdc6e286f48fff7f168537ae8c360e90371a
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-14 16:41:27 +0300

    react refresher course

---
+++ my-app/src/component.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+
+function MyForm(){
+const[name, setName]= useState("");
+
+
+const handleSubmit=(event)=>{
+ event.preventDefault();
+ alert(`The name you entered was: ${name}`)
+}
+
+return(
+<form onSubmit={handleSubmit}>
+    <label>
+        Enter your Name:
+        <input type="text" value={name}
+        onChange={e=>setName(e.target.value)}>
+        </input>
+    </label>
+    <input type="submit"></input>
+    
+</form>
+);
+
+}
+
+export default MyForm;
+  
