e044ad4a47e2b621cd1d33aea23bd9a6b2c16183
Brevian <Wanjalawafulabrevian@gmail.com>
2024-10-14 18:11:24 +0300

    additional

--- my-app/src/component.js
+++ my-app/src/component.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 
 
 function MyForm(){
@@ -21,6 +21,40 @@ return(
     <input type="submit"></input>
     
 </form>
+);
+
+}
+
+export default MyForm;*/
+import React, { useState } from "react";
+
+
+function MyForm(){
+const[inputs, setInputs]= useState({});
+
+
+const handleChange= (event)=>{
+const name = event.target.name;
+const value= event.target.value;
+setInputs(values=>({...values,[name]:value}))
+
+}
+
+const handleSubmit=(event)=>{
+    event.preventDefault()
+    alert("submitted successfully")
+}
+
+return(
+    <form onSubmit={handleSubmit}>
+        <label>Enter your Name :
+            <input  input typeof="text" name="username" value={inputs.username || ""} onChange={handleChange}></input>
+        </label>
+        <label>Enter your Age :
+            <input  input typeof="number" name="age" value={inputs.age || ""} onChange={handleChange}></input>
+        </label>
+        <input type="submit"></input>
+    </form>
 );
 
 }
